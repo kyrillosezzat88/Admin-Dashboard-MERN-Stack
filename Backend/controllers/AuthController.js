934f79b0591347f7739fce6d4f40e01d15527f29
ezzat <ezzat@mauve.de>
2022-12-14 16:58:10 +0200

    change bcrypt versions

--- Backend/controllers/AuthController.js
+++ Backend/controllers/AuthController.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/user')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const Jwt = require('jsonwebtoken');
 //Login 
 const Login = async (req , res) => {
